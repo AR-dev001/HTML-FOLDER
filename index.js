2e463311e1a5c66de6c8764a8e2ee7814ff9648b
AR-dev001 <abdurrafeh2013@gmail.com>
2025-07-04 16:25:08 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import react from 'react';
-import reactDom from 'react-dom';
-ReactDOM.render(<p>hello</p>,document.getElementById('root'));
-
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+class MyStyling extends React.Component{
+  render(){
+    const myheader={
+      color:"green",
+      backgroundColor:"black"
+    };
+    const mystyle={
+      color:"blue",
+      backgroundColor:"yellow"
+    };
+    return(
+      <div>
+        <h1 style={myheader}>Hello</h1>
+        <p style={mystyle}>Hi</p>
+      </div>
+    );
+  }
+}
+const container=document.getElementById("root");
+const root=createRoot(container);
+root.render(<MyStyling/>);
